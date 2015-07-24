4db049eb1de4dfd94601a56acf38801ac9def097
Henrik Lissner <henrik@lissner.net>
2015-07-24 19:12:38 +0200

    prime_factors: optimize py solution

--- numbers/prime_factors/solution.py
+++ numbers/prime_factors/solution.py
@@ -4,44 +4,28 @@
 #   Usage: python solution.py 10 11 12
 
 import sys
-
-def prime_factors_for(n):
-    factors = []
-    while not is_prime(n):
-        p = next_prime_factor(n, 1)
-        factors += [p]
-        n /= p
-    if len(factors) == 0:
-        factors += [1]
-    factors += [int(n)]
-
-    return factors
-
-def next_prime_factor(n=500, last=1):
-    next_num = last + 1
-    if n % next_num == 0 and is_prime(next_num):
-        return next_num
-    if next_num >= n:
-        return False
-    return next_prime_factor(n, next_num)
+from math import sqrt
 
 def is_prime(n):
-    n = int(n)
-    if n == 0:
-        return False
-    if n <= 2:
-        return True
-    if n > 10 and (n % 2 == 0 or n % 5 == 0):
-        return False
-    for i in range(2, n):
+    for i in range(2, int(sqrt(n))+1):
         if n % i == 0:
             return False
     return True
+
+def pfactors(n):
+    factors = [1]
+    if n % 2 == 0:
+        factors.append(2)
+    for x in range(3, int(sqrt(n))+1, 2):
+        if n % x == 0:
+            if is_prime(x):
+                factors.append(x)
+    return factors + [n]
 
 
 argc = len(sys.argv)
 if argc > 1:
     for i in sys.argv[1:]:
-        print(i, "=", prime_factors_for(int(i)))
+        print(pfactors(int(i)))
 else:
     print("Usage {0} [number1] [[number2] ...]")
