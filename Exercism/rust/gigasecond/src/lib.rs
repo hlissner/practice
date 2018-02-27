43564b2fd81d5214290c129e28dd4eaa35712012
Henrik Lissner <henrik@lissner.net>
2018-02-26 22:33:46 -0500

    Add Exercism/rust/gigasecond

---
+++ Exercism/rust/gigasecond/src/lib.rs
@@ -0,0 +1,7 @@
+extern crate chrono;
+use chrono::*;
+
+// Returns a Utc DateTime one billion seconds after start.
+pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    start + Duration::seconds(1_000_000_000)
+}
